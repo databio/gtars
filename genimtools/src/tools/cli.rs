43bba9414d5b5f59dee915abc0a29f299fee719f
Nathan LeRoy <NLeRoy917@gmail.com>
2024-01-18 15:58:55 -0500

    add cli for data dir stat

---
+++ genimtools/src/tools/cli.rs
@@ -0,0 +1,31 @@
+use super::*;
+use clap::{arg, ArgMatches, Command};
+
+pub fn make_prune_cli() -> Command {
+    Command::new(consts::DATA_DIR_STAT_CMD)
+        .author("Nathan LeRoy")
+        .about("Collect data statistics on all bed files in a directory.")
+        .arg(arg!(<PATH> "Path to the data directory.").required(true))
+        .arg(
+            arg!(--out <VALUE> "Path to the output file.")
+                .required(false),
+        )
+}
+
+pub mod handlers {
+    use super::*;
+
+    pub fn data_dir_stat_handler(matches: &ArgMatches) {
+        let path = matches
+            .get_one::<String>("path")
+            .expect("Path is required");
+
+        let out = matches
+            .get_one::<String>("out")
+            .unwrap_or(&consts::DEFAULT_OUTPUT.to_string())
+            .to_owned();
+
+        // core logic/algorithm here
+        data_dir_stat(path, out.as_str());
+    }
+}
--- genimtools/src/tools/mod.rs
+++ genimtools/src/tools/mod.rs
@@ -3,7 +3,14 @@ use std::io::{BufReader, Write, BufRead};
 
 use walkdir::WalkDir;
 
+pub mod cli;
 
+pub mod consts {
+    pub const DATA_DIR_STAT_CMD: &str = "dir-stat";
+    pub const DEFAULT_OUTPUT: &str = "output.tsv";
+}
+
+///
 /// Collect statistics of a given data directory. This
 /// will look through a potential data directory and
 /// report some statistics about the data contained
@@ -19,18 +26,22 @@ use walkdir::WalkDir;
 /// - `out`: The path to the output file.
 ///
 pub fn data_dir_stat(path: &str, out: &str) {
-    // create the writer 
+    // create the writer
     let mut writer = File::create(out).unwrap();
 
+    // iterate over the files in the directory
     for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
         if entry.file_type().is_file() {
 
+            // get the file name, size, and number of lines
             let fname = entry.file_name().to_str().unwrap();
             let size = entry.metadata().unwrap().len();
             let num_lines = BufReader::new(File::open(entry.path()).unwrap()).lines().count();
 
+            // create the line
             let line = format!("{}\t{}\t{}\n", fname, size, num_lines);
 
+            // write the line
             writer.write_all(line.as_bytes()).unwrap();
         }
     }
