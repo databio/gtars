5554b3f7768cbfae1ecb92437080949cbdb3de27
Nathan LeRoy <NLeRoy917@gmail.com>
2024-04-17 17:04:19 -0400

    workon tokenized Region set

--- genimtools/src/common/models/tokenized_region.rs
+++ genimtools/src/common/models/tokenized_region.rs
@@ -1,7 +1,29 @@
+use crate::common::models::Region;
+
 #[derive(Eq, PartialEq, Clone)]
 pub struct TokenizedRegion {
     pub chr: String,
     pub start: u32,
     pub end: u32,
     pub id: u32,
+}
+
+impl From<TokenizedRegion> for Region {
+    fn from(val: TokenizedRegion) -> Self {
+        Region {
+            chr: val.chr,
+            start: val.start,
+            end: val.end,
+        }
+    }
+}
+
+impl From<&TokenizedRegion> for Region {
+    fn from(value: &TokenizedRegion) -> Self {
+        Region {
+            chr: value.chr,
+            start: value.start,
+            end: value.end,
+        }
+    }
 }
