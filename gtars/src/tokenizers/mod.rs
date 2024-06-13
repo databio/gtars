47a33ae6cd23c7240f965a1d803bd48085907e48
Nathan LeRoy <NLeRoy917@gmail.com>
2024-06-13 12:50:55 -0400

    update tests

--- gtars/src/tokenizers/mod.rs
+++ gtars/src/tokenizers/mod.rs
@@ -21,3 +21,58 @@ pub use config::TokenizerConfig;
 pub use fragment_tokenizer::FragmentTokenizer;
 pub use traits::{SingleCellTokenizer, Tokenizer};
 pub use tree_tokenizer::TreeTokenizer;
+
+#[cfg(test)]
+mod tests {
+
+    use crate::common::models::RegionSet;
+    use std::path::Path;
+
+    use super::*;
+    use pretty_assertions::assert_eq;
+    use rstest::*;
+
+    #[fixture]
+    fn path_to_bed_file() -> &'static str {
+        "tests/data/peaks.bed"
+    }
+
+    #[fixture]
+    fn path_to_config_file() -> &'static str {
+        "tests/data/tokenizer.toml"
+    }
+
+    #[fixture]
+    fn path_to_tokenize_bed_file() -> &'static str {
+        "tests/data/to_tokenize.bed"
+    }
+
+    #[rstest]
+    fn test_create_tokenizer_from_bed(path_to_bed_file: &str) {
+        let tokenizer = TreeTokenizer::try_from(Path::new(path_to_bed_file)).unwrap();
+        assert_eq!(tokenizer.vocab_size(), 32); // 25 regions + 7 special tokens
+    }
+
+    #[rstest]
+    fn test_create_tokenizer_from_config(path_to_config_file: &str) {
+        let tokenizer = TreeTokenizer::try_from(Path::new(path_to_config_file)).unwrap();
+        assert_eq!(tokenizer.vocab_size(), 56); // 25 regions in main universe + 24 in hierarchical + 7 special tokens
+    }
+
+    #[rstest]
+    fn test_tokenize_bed_file(path_to_bed_file: &str, path_to_tokenize_bed_file: &str) {
+        let tokenizer = TreeTokenizer::try_from(Path::new(path_to_bed_file)).unwrap();
+        let rs = RegionSet::try_from(Path::new(path_to_tokenize_bed_file)).unwrap();
+        let tokenized_regions = tokenizer.tokenize_region_set(&rs);
+
+        println!("{}", tokenized_regions.len());
+        assert_eq!(tokenized_regions.len(), 4);
+
+        // last should be the unknown token
+        let unknown_token = tokenizer
+            .universe
+            .convert_id_to_region(tokenized_regions[3])
+            .unwrap();
+        assert!(unknown_token.chr == "chrUNK");
+    }
+}
