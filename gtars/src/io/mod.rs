cac526174ed3f6cca4523e723d5b964cda94357f
Nathan LeRoy <NLeRoy917@gmail.com>
2024-06-25 18:49:35 -0400

    update tests for saving tokens.gtok

--- gtars/src/io/mod.rs
+++ gtars/src/io/mod.rs
@@ -11,12 +11,12 @@
 //! use gtars::io::write_tokens_to_gtok;
 //!
 //! let ids = vec![42, 101, 999];
-//! write_tokens_to_gtok("tokens.gtok", &ids);
+//! write_tokens_to_gtok("tests/data/out/tokens.gtok", &ids);
 //! ```
 //! ### Read tokens from disk
 //! ```rust
 //! use gtars::io::read_tokens_from_gtok;
-//! let ids = read_tokens_from_gtok("tokens.gtok").unwrap();
+//! let ids = read_tokens_from_gtok("tests/data/out/tokens.gtok").unwrap();
 //!
 //! println!("{:?}", ids); // [42, 101, 999]
 //! ```
