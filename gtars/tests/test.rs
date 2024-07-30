cdcb93d083bafde402d6aeea7bda4e127c7dcd5f
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2024-07-30 11:42:22 -0400

    cargo fmt

--- gtars/tests/test.rs
+++ gtars/tests/test.rs
@@ -1,11 +1,11 @@
+use std::fs::File;
 use std::io::{BufRead, BufReader, Read};
 use std::path::{Path, PathBuf};
-use std::fs::{File};
 
 use rstest::*;
 use tempfile::tempdir;
 
-use gtars::uniwig::{parse_bed_file};
+use gtars::uniwig::parse_bed_file;
 
 #[fixture]
 fn path_to_data() -> &'static str {
@@ -28,8 +28,8 @@ fn path_to_bed_file_gzipped() -> &'static str {
 }
 
 mod tests {
+    use gtars::uniwig::{read_bed_vec, uniwig_main, Chromosome};
     use std::env::temp_dir;
-    use gtars::uniwig::{Chromosome, read_bed_vec, uniwig_main};
 
     use super::*;
 
@@ -45,7 +45,6 @@ mod tests {
         let result = parse_bed_file(&first_line);
 
         if let Some((ctg, st, en)) = result {
-
             println!("ctg: {}", ctg);
             println!("st: {}", st);
             println!("en: {}", en);
@@ -53,38 +52,32 @@ mod tests {
         } else {
             panic!("Failed to parse BED record");
         }
-
     }
 
     #[rstest]
     fn test_read_bed_vec(path_to_bed_file: &str, path_to_bed_file_gzipped: &str) {
-
         let result1 = read_bed_vec(path_to_bed_file);
-        assert_eq!(result1.len(),20);
+        assert_eq!(result1.len(), 20);
 
         let result2 = read_bed_vec(path_to_bed_file_gzipped);
-        assert_eq!(result2.len(),20);
-
+        assert_eq!(result2.len(), 20);
     }
 
     #[rstest]
     fn test_read_bed_vec_length(path_to_sorted_small_bed_file: &str) {
-
-        let chromosomes: Vec<Chromosome>  = read_bed_vec(path_to_sorted_small_bed_file);
+        let chromosomes: Vec<Chromosome> = read_bed_vec(path_to_sorted_small_bed_file);
         let num_chromosomes = chromosomes.len();
 
         assert_eq!(num_chromosomes, 5);
-
     }
     #[rstest]
     fn test_run_uniwig_main_wig_type(path_to_bed_file: &str) {
+        let path_to_crate = env!("CARGO_MANIFEST_DIR");
 
-        let path_to_crate= env!("CARGO_MANIFEST_DIR");
-
-        let tempbedpath = format!("{} {}",path_to_crate, "/tests/data/test5.bed");
+        let tempbedpath = format!("{} {}", path_to_crate, "/tests/data/test5.bed");
         let combinedbedpath = tempbedpath.as_str();
 
-        let chromsizerefpath: String = format!("{} {}",path_to_crate, "/tests/hg38.chrom.sizes");
+        let chromsizerefpath: String = format!("{} {}", path_to_crate, "/tests/hg38.chrom.sizes");
 
         let tempdir = tempfile::tempdir().unwrap();
         let path = PathBuf::from(&tempdir.path());
@@ -94,21 +87,25 @@ mod tests {
         let bwfileheader = bwfileheader_path.as_str();
 
         let smoothsize: i32 = 5;
-        let output_type ="wig";
+        let output_type = "wig";
 
-        uniwig_main(smoothsize, combinedbedpath, &chromsizerefpath, bwfileheader, output_type)
-
+        uniwig_main(
+            smoothsize,
+            combinedbedpath,
+            &chromsizerefpath,
+            bwfileheader,
+            output_type,
+        )
     }
 
     #[rstest]
     fn test_run_uniwig_main_npy_type(path_to_bed_file: &str) {
+        let path_to_crate = env!("CARGO_MANIFEST_DIR");
 
-        let path_to_crate= env!("CARGO_MANIFEST_DIR");
-
-        let tempbedpath = format!("{} {}",path_to_crate, "/tests/data/test5.bed");
+        let tempbedpath = format!("{} {}", path_to_crate, "/tests/data/test5.bed");
         let combinedbedpath = tempbedpath.as_str();
 
-        let chromsizerefpath: String = format!("{} {}",path_to_crate, "/tests/hg38.chrom.sizes");
+        let chromsizerefpath: String = format!("{} {}", path_to_crate, "/tests/hg38.chrom.sizes");
 
         let tempdir = tempfile::tempdir().unwrap();
         let path = PathBuf::from(&tempdir.path());
@@ -118,9 +115,14 @@ mod tests {
         let bwfileheader = bwfileheader_path.as_str();
 
         let smoothsize: i32 = 5;
-        let output_type ="npy";
+        let output_type = "npy";
 
-        uniwig_main(smoothsize, combinedbedpath, &chromsizerefpath, bwfileheader, output_type)
-
+        uniwig_main(
+            smoothsize,
+            combinedbedpath,
+            &chromsizerefpath,
+            bwfileheader,
+            output_type,
+        )
     }
 }
