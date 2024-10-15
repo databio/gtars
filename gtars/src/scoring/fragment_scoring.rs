649e98b5435cf73c48a27e286cc02a0a9f09eff4
Nathan LeRoy <NLeRoy917@gmail.com>
2024-10-15 13:03:24 -0400

    create more models/structs

--- gtars/src/scoring/fragment_scoring.rs
+++ gtars/src/scoring/fragment_scoring.rs
@@ -1,9 +1,5 @@
-use std::collections::HashMap;
-use std::path::Path;
+use anyhow::Result;
 
-use anyhow::{Context, Result};
-use rust_lapper::{Interval, Lapper};
-
-use crate::common::models::{Region, RegionSet};
-use crate::common::utils::extract_regions_from_bed_file;
-
+pub fn region_scoring() -> Result<()> {
+    Ok(())
+}
