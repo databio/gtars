7be1267d95409c942fe39e7c98ef8ca77b43628b
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2024-11-20 15:41:10 -0500

    begin adding  variable_start_end_counts_bam_to_bw

--- gtars/src/uniwig/counting.rs
+++ gtars/src/uniwig/counting.rs
@@ -816,6 +816,203 @@ pub fn fixed_start_end_counts_bam_to_bw(
     Ok(())
 }
 
+/// Variable counting function, used specifically for bam input to bw output
+/// writes a variable step bedgraph line by line
+pub fn variable_start_end_counts_bam_to_bw(
+    records: &mut Box<Query<noodles::bgzf::reader::Reader<std::fs::File>>>,
+    chrom_size: i32,
+    smoothsize: i32,
+    stepsize: i32,
+    chromosome_name: &String,
+    out_sel: &str,
+    write_fd: Arc<Mutex<PipeWriter>>,
+) -> Result<(), BAMRecordError> {
+
+    let mut write_lock = write_fd.lock().unwrap(); // Acquire lock for writing
+    let mut writer = BufWriter::new(&mut *write_lock);
+
+    let mut v_coordinate_positions: Vec<i32> = Vec::new(); // these are the final coordinates after any adjustments
+    let mut v_coord_counts: Vec<u32> = Vec::new(); // u8 stores 0:255 This may be insufficient. u16 max is 65535
+
+    let mut coordinate_position = 1;
+
+    let mut count: i32 = 0;
+
+    let mut coordinate_value: i32;
+    let mut prev_coordinate_value = 0;
+
+    let mut adjusted_start_site: i32;
+    let mut current_end_site: i32;
+
+    let mut collected_end_sites: Vec<i32> = Vec::new();
+
+    let first_record_option = records.next();
+
+    let first_record = match first_record_option {
+        Some(Ok(record)) => record, // Extract the record
+        Some(Err(err)) => {
+            // Handle the error
+            eprintln!(
+                "Error reading the first record for chrom: {} {:?} Skipping...",
+                chromosome_name, err
+            );
+            writer.write_all(b"\n").unwrap();
+            writer.flush().unwrap();
+            drop(writer);
+            return Err(BAMRecordError::NoFirstRecord); // Example error handling
+        }
+        None => {
+            // Handle no records
+            eprintln!(
+                "Error reading the first record for chrom: {} Skipping...",
+                chromosome_name
+            );
+            writer.write_all(b"\n").unwrap();
+            writer.flush().unwrap();
+            drop(writer);
+            return Err(BAMRecordError::NoFirstRecord);
+        }
+    };
+
+    let mut adjusted_start_site: i32 = match out_sel {
+        "start" => first_record.alignment_start().unwrap().unwrap().get() as i32,
+        "end" => first_record.alignment_end().unwrap().unwrap().get() as i32,
+        _ => {
+            writer.write_all(b"\n").unwrap();
+            writer.flush().unwrap();
+            drop(writer);
+            return Err(BAMRecordError::IncorrectSel); // Example error handling
+            //panic!("unknown output selection must be either 'start', 'end', 'core'")
+        }
+    };
+
+    adjusted_start_site = adjusted_start_site - smoothsize;
+
+    current_end_site = adjusted_start_site;
+    current_end_site = adjusted_start_site + 1 + smoothsize * 2;
+
+    if adjusted_start_site < 1 {
+        adjusted_start_site = 1;
+    }
+
+    while coordinate_position < adjusted_start_site {
+        // Just skip until we reach the initial adjusted start position
+        // Note that this function will not return 0s at locations before the initial start site
+        coordinate_position = coordinate_position + stepsize;
+    }
+
+    for coord in records {
+        let mut coordinate_value: i32 = match out_sel {
+            "start" => coord.unwrap().alignment_start().unwrap().unwrap().get() as i32,
+            "end" => coord.unwrap().alignment_end().unwrap().unwrap().get() as i32,
+            _ => {
+                writer.write_all(b"\n").unwrap();
+                writer.flush().unwrap();
+                return Err(BAMRecordError::IncorrectSel);
+                //panic!("unknown output selection must be either 'start', 'end', 'core'")
+            }
+        };
+
+        // coordinate_value = coord.unwrap().alignment_start().unwrap().unwrap().get() as i32;
+
+        adjusted_start_site = coordinate_value;
+        adjusted_start_site = coordinate_value - smoothsize;
+
+        //let current_score = adjusted_start_site;
+
+        count += 1;
+
+        if adjusted_start_site < 1 {
+            adjusted_start_site = 1;
+        }
+
+        //let current_index = index;
+
+        let mut new_end_site = adjusted_start_site;
+        new_end_site = adjusted_start_site + 1 + smoothsize * 2;
+        collected_end_sites.push(new_end_site);
+
+        if adjusted_start_site == prev_coordinate_value {
+            continue;
+        }
+
+        while coordinate_position < adjusted_start_site {
+
+            while current_end_site == coordinate_position {
+                count = count - 1;
+
+                if count < 0 {
+                    count = 0;
+                }
+
+                if collected_end_sites.last() == None {
+                    current_end_site = 0;
+                } else {
+                    current_end_site = collected_end_sites.remove(0)
+                }
+            }
+
+            if coordinate_position % stepsize == 0 {
+                let single_line = format!(
+                    "{}\t{}\t{}\t{}\n",
+                    chromosome_name,
+                    coordinate_position,
+                    coordinate_position + 1,
+                    count
+                );
+                writer.write_all(single_line.as_bytes())?;
+                writer.flush()?;
+                eprintln!("{}",single_line);
+            }
+
+            coordinate_position = coordinate_position + 1;
+        }
+
+        prev_coordinate_value = adjusted_start_site;
+    }
+
+    count = count + 1; // We must add 1 extra value here so that our calculation during the tail as we close out the end sites does not go negative.
+    // this is because the code above subtracts twice during the INITIAL end site closure. So we are missing one count and need to make it up else we go negative.
+
+    while coordinate_position < chrom_size {
+        // Apply a bound to push the final coordinates otherwise it will become truncated.
+
+        while current_end_site == coordinate_position {
+            let current_score = adjusted_start_site;
+            count = count - 1;
+            if count < 0 {
+                count = 0;
+            }
+
+            if collected_end_sites.last() == None {
+                current_end_site = 0;
+            } else {
+                current_end_site = collected_end_sites.remove(0)
+            }
+        }
+
+        if coordinate_position % stepsize == 0 {
+            // Step size defaults to 1, so report every value
+            let single_line = format!(
+                "{}\t{}\t{}\t{}\n",
+                chromosome_name,
+                coordinate_position,
+                coordinate_position + 1,
+                count
+            );
+            writer.write_all(single_line.as_bytes())?;
+            writer.flush()?;
+            eprintln!("{}",single_line);
+        }
+
+        coordinate_position = coordinate_position + 1;
+    }
+
+    drop(writer);
+
+    Ok(())
+}
+
 fn set_up_file_output(
     output_type: &str,
     adjusted_start_site: i32,
