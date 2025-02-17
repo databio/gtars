77df22164e54355d51d58684fa0f7d51c13676f2
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2025-02-17 16:22:27 -0500

    add creating parent and child directories based on a directory of bedfiles

--- gtars/src/igd/bloom.rs
+++ gtars/src/igd/bloom.rs
@@ -1,5 +1,8 @@
+use std::collections::HashMap;
+use std::fs;
 use std::fs::File;
-use std::io::{Read, Write};
+use std::io::{Error, Read, Write};
+use std::path::Path;
 // for now use simple implementation of bloom filter in rust
 use bloomfilter;
 use bloomfilter::Bloom;
@@ -12,10 +15,27 @@ pub fn create_bloom_filter_main(){
 
     let save_path ="/home/drc/Downloads/bloom_testing/test1/";
     let name = "test";
+    let parent_directory = format!("{}{}/",save_path.clone(),name.clone());
 
-    let universe_file = "";
+    let bed_directory = "/home/drc/Downloads/bloom_testing/test1/all_bed_files/";
 
-    let query_bed_file = "";
+    let mut child_meta_data_map: HashMap<String, String> = HashMap::new(); // File name and directory to store files
+
+    make_parent_directory(parent_directory.as_str()).unwrap();
+    // For now we will store the individual bloom filters in child directories
+    make_child_directories(parent_directory, bed_directory, &mut child_meta_data_map);
+
+    // DEBUG CHECK TO ENSURE WE CHANGED HASHMAP
+    // for (key, value) in child_meta_data_map {  // &map to borrow, not take ownership
+    //     println!("Key: {}, Value: {}", key, value);
+    // }
+
+    // TODO
+    // TODO Based on collected BED files and assuming your universe was created using those same bed files
+    // TODO tokenize the bed files and create individual bloom filters within each folder using the tokenized data
+
+
+
 
     let example_region = "chr1|500|800";
 
@@ -41,6 +61,67 @@ pub fn create_bloom_filter_main(){
     write_bloom_filter_to_disk(igd_bloom_filter, final_save_path.clone());
     load_bloom_filter_from_disk(final_save_path.clone());
 
+}
+
+fn make_child_directories(parent_directory: String, bed_directory: &str, meta_data: &mut HashMap<String, String>) {
+
+    //let mut bed_files = Vec::new();
+    for entry in fs::read_dir(bed_directory).unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+
+        if path.is_file() {
+            if let Some(extension) = path.extension() {
+                if extension == "bed" {
+                    let full_name = path.to_str().unwrap().to_string();
+                    println!("Here is the full name: {}", full_name.clone());
+
+                    let name_without_extension = path
+                        .file_stem()
+                        .unwrap()
+                        .to_str()
+                        .unwrap()
+                        .to_string();
+                    println!("Here is the name without extension: {}", name_without_extension.clone());
+
+
+                    let single_parent_directory = format!("{}{}/",parent_directory,name_without_extension);
+                    make_parent_directory(single_parent_directory.as_str());
+
+                    meta_data.insert(single_parent_directory,full_name.clone());
+
+                }
+            }
+        }
+    }
+
+
+
+
+
+}
+
+pub fn make_parent_directory(parent_directory: &str) -> Result<(), Error> {
+
+
+    let parent_path = Path::new(&parent_directory);
+
+    if !parent_path.exists() {
+
+        match fs::create_dir_all(&parent_directory) {
+            Ok(_) => {println!("Parent directory created successfully: {}", parent_directory);
+                Ok(())
+            }
+            ,
+            Err(e) => {
+                eprintln!("Error creating parent directory: {}", e);
+                return Err(e); // Example: Returning the error
+            }
+        }
+    } else {
+        println!("Parent directory already exists: {}", parent_directory);
+        Ok(())
+    }
 }
 
 pub fn write_bloom_filter_to_disk(igd_bloom_filter: Bloom<&str>, save_path: String){
