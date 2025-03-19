a9c238de6470628eb82dd538435bcf5abbd127fd
Nathan LeRoy <NLeRoy917@gmail.com>
2025-03-19 16:58:17 -0400

    add more classes to module

--- bindings/python/src/tokenizers/mod.rs
+++ bindings/python/src/tokenizers/mod.rs
@@ -5,9 +5,13 @@ mod universe;
 use pyo3::prelude::*;
 
 use crate::tokenizers::py_tokenizers::PyTokenizer;
+use crate::tokenizers::universe::PyUniverse;
+use crate::tokenizers::tokens::PyTokenizedRegionSet;
 
 #[pymodule]
 pub fn tokenizers(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<PyTokenizer>()?;
+    m.add_class::<PyTokenizedRegionSet>()?;
+    m.add_class::<PyUniverse>()?;
     Ok(())
 }
