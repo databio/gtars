680749f7d8afc98b522638e76a8b49905c4e3a5b
Khoroshevskyi <sasha99250@gmail.com>
2025-04-15 14:07:02 -0400

    lint

--- bindings/python/py_src/gtars/tokenizers/__init__.pyi
+++ bindings/python/py_src/gtars/tokenizers/__init__.pyi
@@ -2,9 +2,8 @@ from typing import Any, Dict, List, Optional, Union
 
 class Tokenizer:
     def __init__(self, path: str) -> None: ...
-    
     @classmethod
-    def from_config(cls, cfg: str) -> 'Tokenizer':
+    def from_config(cls, cfg: str) -> "Tokenizer":
         """
         Loads a tokenizer from a configuration file.
         Args:
@@ -12,9 +11,9 @@ class Tokenizer:
         Returns:
             Tokenizer: An instance of the Tokenizer class.
         """
-    
+
     @classmethod
-    def from_bed(cls, path: str) -> 'Tokenizer':
+    def from_bed(cls, path: str) -> "Tokenizer":
         """
         Loads a tokenizer from a BED file.
         Args:
@@ -22,7 +21,7 @@ class Tokenizer:
         Returns:
             Tokenizer: An instance of the Tokenizer class.
         """
-    
+
     def tokenize(self, regions: Any) -> List[str]:
         """
         Tokenizes the input regions into a list of tokens.
@@ -31,7 +30,7 @@ class Tokenizer:
         Returns:
             List[str]: A list of tokens.
         """
-    
+
     def encode(self, tokens: Any) -> Union[List[int], int]:
         """
         Encodes the input tokens into a list of token IDs.
@@ -40,7 +39,7 @@ class Tokenizer:
         Returns:
             Union[List[int], int]: A list of token IDs or a single token ID.
         """
-    
+
     def decode(self, ids: Any) -> Union[List[str], str]:
         """
         Decodes the input token IDs into a list of tokens.
@@ -49,7 +48,7 @@ class Tokenizer:
         Returns:
             Union[List[str], str]: A list of tokens or a single token.
         """
-    
+
     def convert_ids_to_tokens(self, id: Any) -> Union[List[str], str]:
         """
         Converts the input token IDs into a list of tokens.
@@ -58,7 +57,7 @@ class Tokenizer:
         Returns:
             Union[List[str], str]: A list of tokens or a single token.
         """
-    
+
     def convert_tokens_to_ids(self, region: Any) -> Union[List[int], int]:
         """
         Converts the input tokens into a list of token IDs.
@@ -67,52 +66,37 @@ class Tokenizer:
         Returns:
             Union[List[int], int]: A list of token IDs or a single token ID.
         """
-    
-    @property
-    def get_unk_token(self) -> str: ...
 
     @property
+    def get_unk_token(self) -> str: ...
+    @property
     def get_pad_token(self) -> str: ...
-    
     @property
     def get_mask_token(self) -> str: ...
-    
     @property
     def get_cls_token(self) -> str: ...
-    
     @property
     def get_bos_token(self) -> str: ...
-    
     @property
     def get_eos_token(self) -> str: ...
-    
     @property
     def get_sep_token(self) -> str: ...
-    
     @property
     def get_pad_token_id(self) -> int: ...
-    
     @property
     def get_mask_token_id(self) -> int: ...
-    
     @property
     def get_cls_token_id(self) -> int: ...
-    
     @property
     def get_bos_token_id(self) -> int: ...
-    
     @property
     def get_eos_token_id(self) -> int: ...
-    
     @property
     def get_sep_token_id(self) -> int: ...
-    
     @property
     def get_unk_token_id(self) -> int: ...
-    
     @property
     def get_vocab_size(self) -> int: ...
-    
     @property
     def get_special_tokens_map(self) -> Dict[str, Optional[str]]:
         """
@@ -127,11 +111,9 @@ class Tokenizer:
         Returns:
             Dict[str, int]: A dictionary mapping tokens to their IDs.
         """
-    
+
     def __len__(self) -> int: ...
-    
     def __repr__(self) -> str: ...
-    
     def __call__(self, regions: Any) -> Any: ...
 
     class Universe:
@@ -175,18 +157,18 @@ class Tokenizer:
             """
 
 def create_instances(
-        sequences: Union[List[int], List[List[int]]],
-        window_size: int,
-        algorithm: str,
-    ) -> List[Dict[str, Union[int, List[int]]]]:
-        """
-        Creates training instances for a given sequence or list of sequences.
+    sequences: Union[List[int], List[List[int]]],
+    window_size: int,
+    algorithm: str,
+) -> List[Dict[str, Union[int, List[int]]]]:
+    """
+    Creates training instances for a given sequence or list of sequences.
 
-        Args:
-            sequences (Union[List[int], List[List[int]]]): A sequence or list of sequences of token IDs.
-            window_size (int): The size of the context window.
-            algorithm (str): The algorithm to use ('cbow' or 'sg').
+    Args:
+        sequences (Union[List[int], List[List[int]]]): A sequence or list of sequences of token IDs.
+        window_size (int): The size of the context window.
+        algorithm (str): The algorithm to use ('cbow' or 'sg').
 
-        Returns:
-            List[Dict[str, Union[int, List[int]]]]: A list of dictionaries representing the training instances.
-        """
+    Returns:
+        List[Dict[str, Union[int, List[int]]]]: A list of dictionaries representing the training instances.
+    """
