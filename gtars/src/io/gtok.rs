81e2f76d2297267e1850e7631f552bd7af9c03b6
Seth Stadick <sstadick@gmail.com>
2025-07-01 09:58:25 -0400

    feat: example writer

--- gtars/src/io/gtok.rs
+++ gtars/src/io/gtok.rs
@@ -6,6 +6,114 @@ use anyhow::{Context, Result};
 
 use super::consts::{GTOK_HEADER, GTOK_U16_FLAG, GTOK_U32_FLAG};
 
+// A brief impl of Write for GTokenWriter.
+//
+// This allows you to append to your open file instead of doing things in all one shot
+// and exposes it for lower level use via `write` / `write_all`.
+//
+// Additionally, it makes testing easier because you don't have to pass in a file anymore
+// you can just pass in anything that implements Write. This allows allows the user more
+// flexibility for library code.
+//
+// Note, I did not set `pub` on any of this, I think it can all be `pub`, but that could be
+// up to you.
+
+/// Enum for representing the size of tokens in the set.
+#[derive(Debug, Clone)]
+pub enum TokenSize {
+    U16,
+    U32,
+    Unknown,
+}
+
+impl TokenSize {
+    /// Determine the smallest size that can be used for the set.
+    #[inline]
+    fn determine_size(tokens: &[u32]) -> Self {
+        let is_small = tokens.iter().all(|&x| x <= u16::MAX as u32);
+        if is_small {
+            Self::U16
+        } else {
+            Self::U32
+        }
+    }
+
+    /// Convert to a flag for writing.
+    fn as_flag(&self) -> u8 {
+        match self {
+            TokenSize::U16 => GTOK_U16_FLAG,
+            TokenSize::U32 | TokenSize::Unknown => GTOK_U32_FLAG,
+        }
+    }
+}
+
+/// [`GTokWriter`] for writing genomic tokens.
+#[derive(Debug, Clone)]
+pub struct GTokWriter<W: Write> {
+    /// The inner writer.
+    inner: W,
+    /// The expected size for all tokens this will be writing.
+    token_size: TokenSize,
+}
+
+impl<W: Write> GTokWriter<W> {
+    /// Create a new [`GTokWriter`].
+    ///
+    /// This will create the file and write the header, and subsequent calls to write to it will append data.
+    ///
+    /// Size can be set by first calling [`TokenSize::determine_size`] if needed. Use [`TokenSize::Unknown`]
+    /// if the size of all tokens has not been checked.
+    fn new(mut writer: W, token_size: TokenSize) -> Result<Self> {
+        // Header writing could be moved to it's own method
+        writer
+            .write_all(GTOK_HEADER)
+            .with_context(|| "Failed to write GTOK header to file!")?;
+        writer
+            .write_all(&token_size.as_flag().to_le_bytes())
+            .with_context(|| "Failed to write GTOK size flag to file!")?;
+        Ok(Self {
+            inner: writer,
+            token_size,
+        })
+    }
+
+    /// Write multiple tokens at once with a fast path to avoid checking size.
+    fn write_tokens(&mut self, tokens: impl Iterator<Item = u32>) -> Result<()> {
+        match self.token_size {
+            TokenSize::U16 => {
+                for token in tokens {
+                    self.write_all(&(token as u16).to_le_bytes())?
+                }
+            }
+            TokenSize::U32 | TokenSize::Unknown => {
+                for token in tokens {
+                    self.write_all(&token.to_le_bytes())?
+                }
+            }
+        }
+        Ok(())
+    }
+
+    /// Write a single token, checks [`Self`]s token_size.
+    fn write_token(&mut self, token: u32) -> Result<()> {
+        match self.token_size {
+            TokenSize::U16 => self.write_all(&(token as u16).to_le_bytes())?,
+            TokenSize::U32 | TokenSize::Unknown => self.write_all(&token.to_le_bytes())?,
+        };
+        Ok(())
+    }
+}
+
+impl<W: Write> Write for GTokWriter<W> {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.inner.write(buf)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.inner.flush()
+    }
+}
+
 ///
 /// Writes a vector of tokens to a file in the `.gtok` format.
 /// # Arguments
