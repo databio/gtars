adf16f229ecfa8061dc2f072eab9ace5facabd9c
Nathan LeRoy <NLeRoy917@gmail.com>
2025-07-20 11:58:58 -0400

    add some TODOs

--- gtars/src/overlap/traits.rs
+++ gtars/src/overlap/traits.rs
@@ -8,5 +8,7 @@ where
     T: Eq   + Clone    + Send + Sync,
 {
     fn build(intervals: Vec<Interval<I,T>>) -> Self  where Self: Sized;
+    // TODO: how to make this an iterator instead? itearting
+    // over found intervals, i believe, is more performant.
     fn find(&self, start: I, end: I) -> Vec<Interval<I,T>>;
 }
