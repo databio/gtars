d4e6118d6f26a51ef443bd908630180044fe26e0
Nathan LeRoy <NLeRoy917@gmail.com>
2025-07-22 10:58:43 -0400

    start WIP IITree

--- gtars/src/overlap/mod.rs
+++ gtars/src/overlap/mod.rs
@@ -1,9 +1,11 @@
 pub mod traits;
 pub mod bits;
 pub mod ailist;
+pub mod iitree;
 
 
 // re-exports
 pub use self::traits::Overlapper;
 pub use self::bits::Bits;
 pub use self::ailist::AiList;
+pub use self::iitree::IITree;
