0108ad1951e67d8ec5b9a83d2e50d6a9c1a4c736
Nathan LeRoy <NLeRoy917@gmail.com>
2025-08-05 19:04:21 -0400

    add tokenizer logic

--- bindings/wasm/src/lib.rs
+++ bindings/wasm/src/lib.rs
@@ -1,6 +1,16 @@
+mod tokenizers;
 mod utils;
 
+use gtars::{
+    common::models::Region,
+    tokenizers::{
+        config::TokenizerType, create_tokenize_core_from_universe, special_tokens::SpecialTokens,
+        Tokenizer,
+    },
+};
 use wasm_bindgen::prelude::*;
+
+use crate::{tokenizers::BedEntry, utils::prepare_universe_from_bed_entries};
 
 #[wasm_bindgen]
 extern "C" {
@@ -8,6 +18,54 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, gtars!");
+pub fn greet(name: &str) {
+    alert(&format!("Hello from gtars-js, {}", name));
+}
+
+#[wasm_bindgen]
+pub fn tokenize_bed_file_into_universe(
+    universe: &JsValue,
+    to_tokenize: &JsValue,
+    backend: &str,
+) -> Result<JsValue, JsValue> {
+    // get the backend
+    let tokenizer_type = match backend {
+        "ailist" => TokenizerType::AiList,
+        "bits" => TokenizerType::Bits,
+        _ => {
+            return Err(JsValue::from_str(&format!(
+                "Invalid backend specified: {}",
+                backend
+            )))
+        }
+    };
+
+    // destructure the arbitrary js value into something we can work with
+    let universe_entries: Vec<BedEntry> = serde_wasm_bindgen::from_value(universe.clone())?;
+    let to_tokenize_entries: Vec<BedEntry> = serde_wasm_bindgen::from_value(to_tokenize.clone())?;
+
+    let mut universe = prepare_universe_from_bed_entries(universe_entries);
+    let special_tokens = SpecialTokens::default();
+    universe.add_special_tokens(&special_tokens);
+
+    let core = create_tokenize_core_from_universe(&universe, tokenizer_type);
+    let tokenizer = Tokenizer::new(core, universe, special_tokens);
+
+    let regions_to_tokenize = to_tokenize_entries
+        .into_iter()
+        .map(|e| Region {
+            chr: e.chr,
+            start: e.start,
+            end: e.end,
+            rest: None,
+        })
+        .collect::<Vec<Region>>();
+    let tokens = tokenizer.tokenize(&regions_to_tokenize);
+    if tokens.is_err() {
+        return Err(JsValue::from_str("Error tokenizing regions."));
+    }
+    let tokens = tokens.unwrap();
+
+    serde_wasm_bindgen::to_value(&tokens)
+        .map_err(|e| JsValue::from_str(&format!("Error serializing tokens: {}", e)))
 }
--- bindings/wasm/src/utils.rs
+++ bindings/wasm/src/utils.rs
@@ -1,3 +1,8 @@
+use gtars::common::utils::{generate_id_to_region_string_map, generate_region_string_to_id_map};
+use gtars::tokenizers::universe::Universe;
+
+use crate::tokenizers::BedEntry;
+
 pub fn set_panic_hook() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function at least once during initialization, and then
@@ -7,4 +12,26 @@ pub fn set_panic_hook() {
     // https://github.com/rustwasm/console_error_panic_hook#readme
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
+}
+
+// very basic wrapper that is specific to wasm
+// since we can do I/O in a browser and must pass regions
+// to the function in memory
+pub fn prepare_universe_from_bed_entries(region_entries: Vec<BedEntry>) -> Universe {
+    let regions = region_entries
+        .into_iter()
+        .map(|e| format!("{}:{}-{}", e.chr, e.start, e.end))
+        .collect::<Vec<String>>();
+
+    let region_to_id = generate_region_string_to_id_map(&regions);
+    let id_to_region = generate_id_to_region_string_map(&regions);
+
+    Universe {
+        regions,
+        region_to_id,
+        id_to_region,
+        names: None,
+        scores: None,
+        special_tokens: None,
+    }
 }
