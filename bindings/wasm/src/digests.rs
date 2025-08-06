f4d2145e5b5fefa5023f3325271bb13ceda8da4f
Nathan LeRoy <NLeRoy917@gmail.com>
2025-08-06 09:25:44 -0400

    add digests

--- bindings/wasm/src/digests.rs
+++ bindings/wasm/src/digests.rs
@@ -1,18 +1,27 @@
 use wasm_bindgen::prelude::*;
 
-use gtars::refget::fasta::digest_fasta;
+use gtars::refget::{digest::{md5, sha512t24u}, fasta};
 
 #[wasm_bindgen(js_name="digestFasta")]
-pub fn digest_fasta_from_string(fasta: &JsValue) -> Result<JsValue, JsValue> {
+pub fn digest_fasta_from_string(fasta: &JsValue, algorithm: &JsValue) -> Result<JsValue, JsValue> {
 
     let fasta_string: String = serde_wasm_bindgen::from_value(fasta.clone())?;
+    let alg: String = serde_wasm_bindgen::from_value(algorithm.clone())?;
 
-    match digest_fasta(&fasta_string) {
-        Ok(seq_col) => {
-            Ok(JsValue::from_str(&seq_col.digest))
+    let digest = match alg.as_str() {
+        "md5" => {
+            md5(fasta_string)
         },
-        Err(err) => {
-            Err(JsValue::from_str(&format!("There was an error digesting the fasta file: {}", err)))
+        "sha512t24u" => {
+            sha512t24u(fasta_string)
+        },
+        _ => {
+            return Err(JsValue::from_str(&format!(
+                "Invalid algorithm specified: {}",
+                alg
+            )))
         }
-    }
+    };
+   
+    Ok(JsValue::from_str(&digest))
 }
