1c02ce8d26904116e46cf60b4a8c9419f3d49391
Nathan LeRoy <NLeRoy917@gmail.com>
2025-08-22 09:23:43 -0400

    small tweaks to cli

--- gtars-cli/src/main.rs
+++ gtars-cli/src/main.rs
@@ -5,8 +5,8 @@ use clap::Command;
 
 pub mod consts {
     pub const VERSION: &str = env!("CARGO_PKG_VERSION");
-    pub const PKG_NAME: &str = env!("CARGO_PKG_NAME");
-    pub const BIN_NAME: &str = env!("CARGO_PKG_NAME");
+    pub const PKG_NAME: &str = "gtars";
+    pub const BIN_NAME: &str = "gtars";
 }
 
 fn build_parser() -> Command {
