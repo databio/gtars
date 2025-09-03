d7030a5b864a3cbbf168fa30f8f52b4a8ba16ae0
Nathan LeRoy <NLeRoy917@gmail.com>
2025-09-03 10:43:47 -0400

    add models to python bindings

--- gtars-py/src/lib.rs
+++ gtars-py/src/lib.rs
@@ -1,6 +1,7 @@
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
 
+mod models;
 mod utils;
 mod refget;
 mod tokenizers;
@@ -11,10 +12,12 @@ pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 fn gtars(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     let refget_module = pyo3::wrap_pymodule!(refget::refget);
     let tokenize_module = pyo3::wrap_pymodule!(tokenizers::tokenizers);
+    let models_module = pyo3::wrap_pymodule!(models::models);
     let utils_module = pyo3::wrap_pymodule!(utils::utils);
 
     m.add_wrapped(refget_module)?;
     m.add_wrapped(tokenize_module)?;
+    m.add_wrapped(models_module)?;
     m.add_wrapped(utils_module)?;
 
     let sys = PyModule::import_bound(py, "sys")?;
@@ -24,6 +27,7 @@ fn gtars(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     // set names of submodules
     sys_modules.set_item("gtars.refget", m.getattr("refget")?)?;
     sys_modules.set_item("gtars.tokenizers", m.getattr("tokenizers")?)?;
+    sys_modules.set_item("gtars.models", m.getattr("models")?)?;
     sys_modules.set_item("gtars.utils", m.getattr("utils")?)?;
 
     // add constants
