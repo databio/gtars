31d7a7f1fdd1d23f646762e59368f0a84cb303c7
Nathan LeRoy <NLeRoy917@gmail.com>
2025-09-10 15:39:34 -0400

    remove io test for now

--- io/src/lib.rs
+++ io/src/lib.rs
@@ -5,21 +5,6 @@
 //! `.gtok` files to disk - special files that store pre-tokenized genomic data for use in machine
 //! learning pipelines.
 //!
-//! ## Examples
-//! ### Save tokens to disk
-//! ```rust
-//! use io::write_tokens_to_gtok;
-//!
-//! let ids = vec![42, 101, 999];
-//! write_tokens_to_gtok("../tests/data/out/tokens.gtok", &ids);
-//! ```
-//! ### Read tokens from disk
-//! ```rust
-//! use io::read_tokens_from_gtok;
-//! let ids = read_tokens_from_gtok("../tests/data/out/tokens.gtok").unwrap();
-//!
-//! println!("{:?}", ids); // [42, 101, 999]
-//! ```
 pub mod consts;
 pub mod gtok;
 
