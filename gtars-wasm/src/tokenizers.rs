3d0eabb65169b7ab3734b4c9106d0823d36867d3
Nathan LeRoy <NLeRoy917@gmail.com>
2025-10-03 10:22:33 -0400

    lay out initial structure

--- gtars-wasm/src/tokenizers.rs
+++ gtars-wasm/src/tokenizers.rs
@@ -42,4 +42,124 @@ impl JsTokenizer {
 
         Ok(JsTokenizer { internal:tokenizer })
     }
+
+    pub fn tokenize(&self, regions: &JsValue) -> Result<JsValue, JsValue> {
+        let regions: BedEntries = serde_wasm_bindgen::from_value(regions.clone())?;
+        let tokens = self.internal.tokenize(&regions);
+        serde_wasm_bindgen::to_value(&tokens).map_err(|e| JsValue::from_str(&e.to_string()))
+    }
+
+    pub fn encode(&self, tokens: &JsValue) -> Result<JsValue, JsValue> {
+        let encoded = self.internal.encode(tokens);
+        serde_wasm_bindgen::to_value(&encoded).map_err(|e| JsValue::from_str(&e.to_string()))
+    }
+
+    pub fn decode(&self, ids: &JsValue) -> Result<JsValue, JsValue> {
+        let decoded = self.internal.decode(ids);
+        serde_wasm_bindgen::to_value(&decoded).map_err(|e| JsValue::from_str(&e.to_string()))
+    }
+
+    #[wasm_bindgen(js_name = "convertIdsToTokens")]
+    pub fn convert_ids_to_tokens(&self, ids: &JsValue) -> Result<JsValue, JsValue> {
+        let tokens = self.internal.convert_ids_to_tokens(ids);
+        serde_wasm_bindgen::to_value(&tokens).map_err(|e| JsValue::from_str(&e.to_string()))
+    }
+
+    #[wasm_bindgen(js_name = "convertTokensToIds")]
+    pub fn convert_tokens_to_ids(&self, tokens: &JsValue) -> Result<JsValue, JsValue> {
+        let ids = self.internal.convert_tokens_to_ids(tokens);
+        serde_wasm_bindgen::to_value(&ids).map_err(|e| JsValue::from_str(&e.to_string()))
+    }
+
+    #[wasm_bindgen(getter, js_name = "unkToken")]
+    pub fn unk_token(&self) -> String {
+        self.internal.get_unk_token().to_string()
+    }
+
+    #[wasm_bindgen(getter, js_name = "padToken")]
+    pub fn pad_token(&self) -> String {
+        self.internal.get_pad_token().to_string()
+    }
+
+    #[wasm_bindgen(getter, js_name = "maskToken")]
+    pub fn mask_token(&self) -> String {
+        self.internal.get_mask_token().to_string()
+    }
+
+    #[wasm_bindgen(getter, js_name = "clsToken")]
+    pub fn cls_token(&self) -> String {
+        self.internal.get_cls_token().to_string()
+    }
+
+    #[wasm_bindgen(getter, js_name = "bosToken")]
+    pub fn bos_token(&self) -> String {
+        self.internal.get_bos_token().to_string()
+    }
+
+    #[wasm_bindgen(getter, js_name = "eosToken")]
+    pub fn eos_token(&self) -> String {
+        self.internal.get_eos_token().to_string()
+    }
+
+    #[wasm_bindgen(getter, js_name = "sepToken")]
+    pub fn sep_token(&self) -> String {
+        self.internal.get_sep_token().to_string()
+    }
+
+    #[wasm_bindgen(getter, js_name = "padTokenId")]
+    pub fn pad_token_id(&self) -> u32 {
+        self.internal.get_pad_token_id()
+    }
+
+    #[wasm_bindgen(getter, js_name = "maskTokenId")]
+    pub fn mask_token_id(&self) -> u32 {
+        self.internal.get_mask_token_id()
+    }
+
+    #[wasm_bindgen(getter, js_name = "clsTokenId")]
+    pub fn cls_token_id(&self) -> u32 {
+        self.internal.get_cls_token_id()
+    }
+
+    #[wasm_bindgen(getter, js_name = "bosTokenId")]
+    pub fn bos_token_id(&self) -> u32 {
+        self.internal.get_bos_token_id()
+    }
+
+    #[wasm_bindgen(getter, js_name = "eosTokenId")]
+    pub fn eos_token_id(&self) -> u32 {
+        self.internal.get_eos_token_id()
+    }
+
+    #[wasm_bindgen(getter, js_name = "sepTokenId")]
+    pub fn sep_token_id(&self) -> u32 {
+        self.internal.get_sep_token_id()
+    }
+
+    #[wasm_bindgen(getter, js_name = "unkTokenId")]
+    pub fn unk_token_id(&self) -> u32 {
+        self.internal.get_unk_token_id()
+    }
+
+    #[wasm_bindgen(getter, js_name = "vocabSize")]
+    pub fn vocab_size(&self) -> usize {
+        self.internal.get_vocab_size()
+    }
+
+    #[wasm_bindgen(js_name = "specialTokensMap")]
+    pub fn special_tokens_map(&self) -> Result<JsValue, JsValue> {
+        let map = self.internal.get_special_tokens();
+        serde_wasm_bindgen::to_value(&map).map_err(|e| JsValue::from_str(&e.to_string()))
+    }
+
+    #[wasm_bindgen(js_name = "getVocab")]
+    pub fn get_vocab(&self) -> Result<JsValue, JsValue> {
+        let vocab = self.internal.get_vocab();
+        serde_wasm_bindgen::to_value(&vocab).map_err(|e| JsValue::from_str(&e.to_string()))
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn length(&self) -> usize {
+        self.internal.get_universe().len()
+    }
 }
