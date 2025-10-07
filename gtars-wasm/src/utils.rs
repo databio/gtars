7719ab007e8d3a249744c73764684cceaf7d9e93
Nathan LeRoy <NLeRoy917@gmail.com>
2025-10-07 12:01:38 -0400

    add dead code warning to set_panic_hook

--- gtars-wasm/src/utils.rs
+++ gtars-wasm/src/utils.rs
@@ -3,6 +3,7 @@ use gtars_tokenizers::Universe;
 
 use crate::models::BedEntries;
 
+#[allow(dead_code)]
 pub fn set_panic_hook() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function at least once during initialization, and then
