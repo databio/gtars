2d739bd74f07537c29ec32dcd8367c9fdac5a18d
Nathan LeRoy <NLeRoy917@gmail.com>
2025-10-11 11:16:32 -0400

    fix naming

--- gtars-wasm/src/regionset.rs
+++ gtars-wasm/src/regionset.rs
@@ -1,22 +1,17 @@
-use std::collections::HashMap;
-
-use gtars_core::models::Region;
 use wasm_bindgen::prelude::*;
-use crate::models::BedEntriesFull;
-
 
 #[wasm_bindgen(js_name = "RegionSet")]
-pub struct RegionSet {
+pub struct JsRegionSet {
     internal: String,
 }
 
 #[wasm_bindgen(js_class = "RegionSet")]
-impl RegionSet {
+impl JsRegionSet {
 
     #[wasm_bindgen(constructor)]
-    pub fn new(word: &str) -> Result<RegionSet, JsValue> {
+    pub fn new(word: &str) -> Result<JsRegionSet, JsValue> {
         println!("Phineas is ready");
-        Ok(RegionSet {internal: String::from(word)})
+        Ok(JsRegionSet {internal: String::from(word)})
     }
 
     #[wasm_bindgen(getter, js_name = "get_region")]
@@ -26,6 +21,6 @@ impl RegionSet {
 
     #[wasm_bindgen(getter, js_name = "get_phi")]
     pub fn get_phi(&mut self) -> String{
-        return String::from("phi-neaaas");
+        String::from("phi-neaaas")
     }
 }
