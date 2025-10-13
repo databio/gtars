e6dcc4f69df5fcc04dff4cc8239b634a0942004d
Khoroshevskyi <sasha99250@gmail.com>
2025-10-13 14:05:04 -0400

    Added rest columns to the wasm bedentries struct

--- gtars-wasm/src/models.rs
+++ gtars-wasm/src/models.rs
@@ -1,7 +1,4 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
-pub struct BedEntries(pub Vec<(String, u32, u32)>);
-
-#[derive(Serialize, Deserialize)]
-pub struct BedEntriesFull(pub Vec<(String, u32, u32, String)>);
+pub struct BedEntries(pub Vec<(String, u32, u32, String)>);
