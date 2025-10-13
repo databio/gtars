a30edfffbf1796c0a94411b6ebb8c92cbc86c443
Khoroshevskyi <sasha99250@gmail.com>
2025-10-13 15:08:31 -0400

    work on regionset

--- gtars-wasm/src/regionset.rs
+++ gtars-wasm/src/regionset.rs
@@ -1,23 +1,42 @@
 use wasm_bindgen::prelude::*;
+use gtars_core::models::{Region, RegionSet};
+use crate::models::BedEntries;
 
 #[wasm_bindgen(js_name = "RegionSet")]
 pub struct JsRegionSet {
-    internal: String,
+   region_set: RegionSet,
 }
 
 #[wasm_bindgen(js_class = "RegionSet")]
 impl JsRegionSet {
+    // #[wasm_bindgen(constructor)]
+    // pub fn new(word: &str) -> Result<JsRegionSet, JsValue> {
+    //     println!("Phineas is ready");
+    //     Ok(JsRegionSet {
+    //         internal: String::from(word),
+    //     })
+    // }
     #[wasm_bindgen(constructor)]
-    pub fn new(word: &str) -> Result<JsRegionSet, JsValue> {
-        println!("Phineas is ready");
-        Ok(JsRegionSet {
-            internal: String::from(word),
-        })
+    pub fn new(regions: &JsValue) -> Result<JsRegionSet, JsValue> {
+        let regions: BedEntries = serde_wasm_bindgen::from_value(regions.clone())?;
+        let regions: Vec<Region> = regions
+            .0
+            .into_iter()
+            .map(|be| Region {
+                chr: be.0,
+                start: be.1,
+                end: be.2,
+                rest: None,
+            })
+            .collect::<Vec<Region>>();
+        Ok(JsRegionSet { region_set: RegionSet::from(regions) })
     }
 
     #[wasm_bindgen(getter, js_name = "get_region")]
-    pub fn get_region(&mut self) -> String {
-        return self.internal.clone();
+    pub fn get_region(self) -> String {
+        // alert(&format!("Number of regions:, {}!", self.region_set.len()));
+        println!("{}", self.region_set.len());
+        format!("{}", self.region_set.len())
     }
 
     #[wasm_bindgen(getter, js_name = "get_phi")]
