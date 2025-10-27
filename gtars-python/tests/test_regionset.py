c1a566d89750812b624d29c53e21b13293ab7974
Nathan LeRoy <NLeRoy917@gmail.com>
2025-10-27 14:11:39 -0400

    fix tests

--- gtars-python/tests/test_regionset.py
+++ gtars-python/tests/test_regionset.py
@@ -1,33 +1,31 @@
-# import os
-# from pathlib import Path
+import os
+from pathlib import Path
 
-# import pytest
+import pytest
 
-# from gtars.models import RegionSet
+from gtars.models import RegionSet
 
 
-# class TestRegionSet:
-#     @pytest.mark.parametrize(
-#         "bed_file",
-#         [
-#             "https://raw.githubusercontent.com/databio/gtars/refs/heads/master/gtars/tests/data/regionset/dummy.narrowPeak",
-#         ],
-#     )
-#     def test_mean_region_width(self, bed_file):
-#         rs = RegionSet(bed_file)
+class TestRegionSet:
+    @pytest.mark.parametrize(
+        "bed_file",
+        [
+            "https://raw.githubusercontent.com/databio/gtars/refs/heads/master/tests/data/regionset/dummy.narrowPeak",
+        ],
+    )
+    def test_mean_region_width(self, bed_file):
+        rs = RegionSet(bed_file)
 
-#         assert rs.mean_region_width() == 4.22
+        assert rs.mean_region_width() == 4.22
 
-#     # @pytest
-#     @pytest.mark.skip(reason="Failing?")
-#     def test_from_regions(self):
-#         from gtars.models import Region
+    def test_from_regions(self):
+        from gtars.models import Region
 
-#         regions = [
-#             Region(chrom="chr1", start=14, end=514),
-#             Region(chrom="chr19", start=19, end=810),
-#         ]
-#         rs = RegionSet.from_regions(regions)
+        regions = [
+            Region(chr="chr1", start=14, end=514, rest=None),
+            Region(chr="chr19", start=19, end=810, rest=None),
+        ]
+        rs = RegionSet.from_regions(regions)
 
-#         assert isinstance(rs, RegionSet)
-#         assert len(rs) == 2
+        assert isinstance(rs, RegionSet)
+        assert len(rs) == 2
