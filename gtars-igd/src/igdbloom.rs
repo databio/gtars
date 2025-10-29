2ee217c7ea8ba7b87e0b874bb780816d68f64f53
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2025-10-29 17:11:02 -0600

    fmt

--- gtars-igd/src/igdbloom.rs
+++ gtars-igd/src/igdbloom.rs
@@ -1,17 +1,19 @@
+use gtars_core::models::{Region, RegionSet};
+use gtars_tokenizers::tokenizer::Tokenizer;
 use std::collections::HashMap;
 use std::fs;
 use std::io::{Error, Read, Write};
 use std::path::Path;
-use gtars_core::models::{Region, RegionSet};
-use gtars_tokenizers::tokenizer::Tokenizer;
-
 
 #[cfg(feature = "bloom")]
 use bloomfilter::Bloom;
 
 #[cfg(feature = "bloom")]
-fn make_child_directories(parent_directory: String, bed_directory: &str, meta_data: &mut HashMap<String, String>) {
-
+fn make_child_directories(
+    parent_directory: String,
+    bed_directory: &str,
+    meta_data: &mut HashMap<String, String>,
+) {
     //let mut bed_files = Vec::new();
     for entry in fs::read_dir(bed_directory).unwrap() {
         let entry = entry.unwrap();
@@ -20,39 +22,40 @@ fn make_child_directories(parent_directory: String, bed_directory: &str, meta_da
         if path.is_file() {
             //println!("Found a file....");
             if let Some(extension) = path.extension() {
-                if let Some(extension) = extension.to_str(){
+                if let Some(extension) = extension.to_str() {
                     //println!("Found this extension: {}", extension);
-                match extension{
-                    "bed" | "gz" => {
-                        let full_name = path.to_str().unwrap().to_string();
-                        //println!("Here is the full name: {}", full_name.clone());
+                    match extension {
+                        "bed" | "gz" => {
+                            let full_name = path.to_str().unwrap().to_string();
+                            //println!("Here is the full name: {}", full_name.clone());
 
-                        let name_without_extension = path
-                            .file_stem()
-                            .unwrap()
-                            .to_str()
-                            .unwrap()
-                            .to_string();
-                        //println!("Here is the name without extension: {}", name_without_extension.clone());
+                            let name_without_extension =
+                                path.file_stem().unwrap().to_str().unwrap().to_string();
+                            //println!("Here is the name without extension: {}", name_without_extension.clone());
 
+                            let single_parent_directory =
+                                format!("{}{}/", parent_directory, name_without_extension);
+                            let _ = make_parent_directory(single_parent_directory.as_str());
 
-                        let single_parent_directory = format!("{}{}/",parent_directory,name_without_extension);
-                        let _ = make_parent_directory(single_parent_directory.as_str());
+                            meta_data.insert(single_parent_directory, full_name.clone());
+                        }
 
-                        meta_data.insert(single_parent_directory,full_name.clone());
+                        _ => {}
                     }
-
-                    _ => {}
-                }}
-
+                }
             }
         }
     }
 }
 
-
 #[cfg(feature = "bloom")]
-pub fn tokenize_then_create_bloom_for_each_file(universe_tokenizer: &Tokenizer, bed_file: &str, child_directory: &str, num_of_items: usize, false_positive_rate: f64){
+pub fn tokenize_then_create_bloom_for_each_file(
+    universe_tokenizer: &Tokenizer,
+    bed_file: &str,
+    child_directory: &str,
+    num_of_items: usize,
+    false_positive_rate: f64,
+) {
     // we must first tokenize against a universe and then create a bloom filter for each chromosome
     // from that tokenization
 
@@ -65,10 +68,7 @@ pub fn tokenize_then_create_bloom_for_each_file(universe_tokenizer: &Tokenizer, 
 
     let path = Path::new(bed_file);
 
-
-    let filename = path.file_name()
-
-        .and_then(|os_str| os_str.to_str()).unwrap();
+    let filename = path.file_name().and_then(|os_str| os_str.to_str()).unwrap();
 
     // Create bloom filter for each chromosome
 
@@ -77,11 +77,10 @@ pub fn tokenize_then_create_bloom_for_each_file(universe_tokenizer: &Tokenizer, 
     // Check if bloom filter already exists
     if file_exists(&bloom_filter_path) {
         println!("File already exists: {}", bloom_filter_path);
-
-    } else{
-
+    } else {
         // Create new bloom filter
-        let mut current_bloom_filter = Bloom::new_for_fp_rate( num_of_items as usize, false_positive_rate as f64).unwrap();
+        let mut current_bloom_filter =
+            Bloom::new_for_fp_rate(num_of_items as usize, false_positive_rate as f64).unwrap();
 
         // Tokenize regions for this chromosome and add tokens to bloom filter
         let tokenized_regions = universe_tokenizer.tokenize(&regions.regions).unwrap();
@@ -90,27 +89,22 @@ pub fn tokenize_then_create_bloom_for_each_file(universe_tokenizer: &Tokenizer, 
         }
 
         write_bloom_filter_to_disk(current_bloom_filter, bloom_filter_path);
-
     }
-
-
 }
-
-
 
 #[cfg(feature = "bloom")]
 pub fn make_parent_directory(parent_directory: &str) -> Result<(), Error> {
-
-
     let parent_path = Path::new(&parent_directory);
 
     if !parent_path.exists() {
-
         match fs::create_dir_all(&parent_directory) {
-            Ok(_) => {println!("Parent directory created successfully: {}", parent_directory);
+            Ok(_) => {
+                println!(
+                    "Parent directory created successfully: {}",
+                    parent_directory
+                );
                 Ok(())
             }
-            ,
             Err(e) => {
                 eprintln!("Error creating parent directory: {}", e);
                 return Err(e); // Example: Returning the error
@@ -123,8 +117,10 @@ pub fn make_parent_directory(parent_directory: &str) -> Result<(), Error> {
 }
 
 #[cfg(feature = "bloom")]
-pub fn write_bloom_filter_to_disk(igd_bloom_filter: Bloom<String>, save_path: String) -> Result<(), std::io::Error>{
-
+pub fn write_bloom_filter_to_disk(
+    igd_bloom_filter: Bloom<String>,
+    save_path: String,
+) -> Result<(), std::io::Error> {
     let bytes = igd_bloom_filter.to_bytes();
 
     match std::fs::write(&save_path, bytes) {
@@ -137,12 +133,12 @@ pub fn write_bloom_filter_to_disk(igd_bloom_filter: Bloom<String>, save_path: St
             Err(e)
         }
     }
-
-
 }
 
 #[cfg(feature = "bloom")]
-pub fn load_bloom_filter_from_disk(load_path: &str) -> Result<Bloom<String>, Box<dyn std::error::Error>> {
+pub fn load_bloom_filter_from_disk(
+    load_path: &str,
+) -> Result<Bloom<String>, Box<dyn std::error::Error>> {
     let bytes = std::fs::read(load_path)?;
 
     let filter = Bloom::from_bytes(bytes).map_err(|e| format!("Bloom filter error: {}", e))?;
@@ -151,22 +147,18 @@ pub fn load_bloom_filter_from_disk(load_path: &str) -> Result<Bloom<String>, Box
     Ok(filter)
 }
 
-
 fn file_exists(path: &str) -> bool {
     Path::new(path).exists() && Path::new(path).is_file()
 }
 
-
-
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
     use rstest::rstest;
     use std::path::PathBuf;
 
     #[rstest]
-    fn test_bloom_filter(){
-
+    fn test_bloom_filter() {
         let path_to_crate = env!("CARGO_MANIFEST_DIR");
 
         let tempbedpath = PathBuf::from(path_to_crate)
@@ -183,16 +175,21 @@ mod tests{
         let num_of_items = 1000;
         let false_positive_rate = 0.5;
 
-        let tokenizer =
-            Tokenizer::from_auto(bed_path.as_ref()).expect("Failed to create tokenizer from config.");
+        let tokenizer = Tokenizer::from_auto(bed_path.as_ref())
+            .expect("Failed to create tokenizer from config.");
 
-        tokenize_then_create_bloom_for_each_file(&tokenizer, &bed_path, &child_directory, num_of_items, false_positive_rate);
+        tokenize_then_create_bloom_for_each_file(
+            &tokenizer,
+            &bed_path,
+            &child_directory,
+            num_of_items,
+            false_positive_rate,
+        );
 
         // Can we load the bloom filter?
 
         let path = Path::new(bed_path.as_ref());
-        let filename = path.file_name()
-            .and_then(|os_str| os_str.to_str()).unwrap();
+        let filename = path.file_name().and_then(|os_str| os_str.to_str()).unwrap();
         let bloom_filter_path = format!("{}/{}.bloom", child_directory, filename);
 
         let loaded_filter = load_bloom_filter_from_disk(bloom_filter_path.as_str()).unwrap();
@@ -202,11 +199,5 @@ mod tests{
 
         let result = loaded_filter.check(&"chr1:23-31".to_string());
         pretty_assertions::assert_eq!(false, result);
-
     }
-
-
-
 }
-
-
